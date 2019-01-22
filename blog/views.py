d973f8eb4b531347aa7c600ebbd67de6d03e34c7
Cyrille YAO <cyaoyapi@gmail.com>
2019-01-22 04:44:09 +0000

    Add basic view home

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,12 @@
+from django.http inmport HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+def home(request):
+	"""Basic view : Tell Welcome to the visitor."""
+
+	welcome_msg = """<h1>Welcome to our blog!</h1>
+	<p>The Breton pancakes are the best.<p/>
+	"""
+	return HttpResponse(welcome_msg) 

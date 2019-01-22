f23a0b08e615ba071ff446099a260f08baf9388f
Cyrille YAO <cyaoyapi@gmail.com>
2019-01-22 05:13:18 +0000

    Best organisation of urls managing

--- breton_pancakes/urls.py
+++ breton_pancakes/urls.py
@@ -14,11 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
-from blog import views
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accueil', views.home)
+    path('blog/', include('blog.urls')),
 ]
